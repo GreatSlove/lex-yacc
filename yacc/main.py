822d82767d2309925fa6dd43f448185bec2153a1
GreatSlove <505909828@qq.com>
2024-05-22 19:46:31 +0800

    完成parser类的封装,不过仍需修改

--- yacc/main.py
+++ yacc/main.py
@@ -1,99 +1,15 @@
 from typing import List, Tuple
-
-terminal = []
-start = ""
-Producer = Tuple[str, List[str]]
-producer_list: List[Producer] = []
-program2 = ""
-
-def define_rules(ln):
-    global start,terminal
-    len_ln = len(ln)
-    i = 0
-    left = ""
-    right = ""
-    
-    if ln[i] == "%" and ln[i + 1] != "%":
-        i += 1
-        while i < len_ln and ln[i] == " ":
-            i += 1
-        while i < len_ln and ln[i] != " ":
-            left += ln[i]
-            i += 1
-        while i < len_ln and ln[i] == " ":
-            i += 1
-
-        if left == "token":
-            while i < len_ln:
-                if ln[i] != " " and ln[i] != "\n":
-                    right += ln[i]
-                else:
-                    if right:
-                        terminal.append(right)
-                        right = ""
-                i += 1
-            if right:
-                terminal.append(right)
-        elif left == "start":
-            start = ln[i:].strip()
-        else:
-            raise Exception("Unrecognized directive: " + left)
-
-def parse_production(ln, current_production):
-    ln = ln.strip()
-    rights =ln[1:]
-    rights = rights.strip()
-    for right in rights.split(' '):
-        right = right.strip()
-        producer_list.append((current_production,right))
-
-def init_all(filename):
-    current_production = None
-    with open(filename, "r") as ifile:
-        lines=ifile.readlines()
-        i=0
-        while i<len(lines):
-            j=1
-            if lines[i]=="\n":
-                print('blank line')
-                i+=1
-                continue
-            elif lines[i].strip()==";":
-                print('遇到;')
-                i+=1
-                continue
-            elif lines[i].strip()=="":
-                print('blank line')
-                i+=1
-                continue
-            elif lines[i].startswith("%"):
-                print('阶段1')
-                define_rules(lines[i])
-                i+=1
-                continue
-            elif len(lines[i].split())==1:
-                print('阶段2')
-                current_production=lines[i].strip()
-                while lines[j+i].strip()!=';':
-                    print('j='+str(j))
-                    print(current_production)
-
-                    parse_production(lines[j+i],current_production)
-                    j+=1
-            i=i+j
-         
-
-                
+from yaccParser import *
                 
 
 def main():
-    filename = "./yacc/c99.y"
-    init_all(filename)
-    print("Terminals:", terminal)
-    print("Start Symbol:", start)
+    yyl = YaccParser("./yacc/c99.y")
+    
+    print("Terminals:", yyl.terminal)
+    print("Start Symbol:", yyl.start)
     print("Productions:")
-    for production in producer_list:
+    for production in yyl.producer_list:
         print(production)
-
+    
 
 main()
--- yacc/yaccParser.py
+++ yacc/yaccParser.py
@@ -1,20 +1,22 @@
 from typing import List, Tuple
 
 class YaccParser:
+    
     def __init__(self,filename):
         self.terminal = []
         self.start = ""
         self.Producer = Tuple[str, List[str]]
-        self.producer_list: List[Producer] = []
+        self.producer_list: List[self.Producer] = []
         self.program2 = ""
         self.init_all(filename)
 
-    def define_rules(self, ln):
-        left = ""
-        right = ""
+    def define_rules(self,ln):
+        global start,terminal
         len_ln = len(ln)
         i = 0
-        flag = True
+        left = ""
+        right = ""
+        
         if ln[i] == "%" and ln[i + 1] != "%":
             i += 1
             while i < len_ln and ln[i] == " ":
@@ -27,21 +29,58 @@ class YaccParser:
 
             if left == "token":
                 while i < len_ln:
-                    if ln[i] != " ":
+                    if ln[i] != " " and ln[i] != "\n":
                         right += ln[i]
-                        if i == len_ln - 1:
-                            self.terminal.append(right)
                     else:
-                        self.terminal.append(right)
-                        right = ""
+                        if right:
+                            self.terminal.append(right)
+                            right = ""
                     i += 1
+                if right:
+                    self.terminal.append(right)
             elif left == "start":
-                self.start = ln[i:].strip()
+                start = ln[i:].strip()
             else:
-                raise Exception("")
-            
-            
-    def init_all(self, filename):
+                raise Exception("Unrecognized directive: " + left)
+
+    def parse_production(self,ln, current_production):
+        ln = ln.strip()
+        rights =ln[1:]
+        rights = rights.strip()
+        for right in rights.split(' '):
+            right = right.strip()
+            self.producer_list.append((current_production,right))
+
+    def init_all(self,filename):
+        current_production = None
         with open(filename, "r") as ifile:
-            self.read_from_stream(ifile)
-        self.last_deal()
+            lines=ifile.readlines()
+            i,k=0,0
+            while i<len(lines):
+                if lines[i]=="\n":
+                    print('blank line')
+                    i+=1
+                    continue
+                elif lines[i].strip()==";":
+                    print('遇到;')
+                    i+=1
+                    continue
+                elif lines[i].strip()=="":
+                    print('blank line')
+                    i+=1
+                    continue
+                elif lines[i].startswith("%"):
+                    print('阶段1')
+                    self.define_rules(lines[i])
+                    i+=1
+                    continue
+                elif len(lines[i].split())==1:
+                    print('阶段2')
+                    current_production=lines[i].strip()
+                    while lines[j+i].strip()!=';':
+                        print('j='+str(j))
+                        print(current_production)
+
+                        self.parse_production(lines[j+i],current_production)
+                        j+=1
+                i=i+j
