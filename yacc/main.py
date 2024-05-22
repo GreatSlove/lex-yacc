a11a0d054aa5e0a1edb41a758a44ac2a47202837
GreatSlove <505909828@qq.com>
2024-05-22 15:14:25 +0800

    暂存，bug未知

--- yacc/main.py
+++ yacc/main.py
@@ -1,32 +1,3 @@
-def main():
-    # 打开名为 "example.txt" 的文件
-    with open('./yacc/test.txt', 'r') as file:
-    # 逐行读取文件内容
-      for line in file:
-        # 输出每一行
-        print(line.strip())  # 使用 strip() 方法去除每行末尾的换行符
-
-def out():
-   with open('./yacc/test.txt', 'r') as file:
-      for ln in file:
-         for i in ln[i]:
-            print(ln[i])
-        
-
-def out1():
-   with open('./yacc/test.txt', 'r') as file:  # 添加编码格式
-      for ln in file:  # 遍历文件中的每一行
-         for char in ln:  # 遍历当前行中的每个字符
-            print(char)
-
-def out2():
-   with open('./yacc/test.txt', 'r', encoding='utf-8') as file:
-      for ln in file:
-         words = ln.split()  # 将行按空格分割成单词
-         for i in range(len(words)):  # 使用索引遍历单词列表
-            print(words[i])  # 输出索引和对应的单词
-
-
 terminal = []
 start = ""
 Producer = tuple[str, list[str]]
@@ -34,7 +5,6 @@ producer_list: list[Producer] = []
 program2 = ""
 
 def define_rules(ln):
-    global start
     len_ln = len(ln)
     i = 0
     left = ""
@@ -68,25 +38,14 @@ def define_rules(ln):
 
 def parse_production(ln, current_production):
     ln = ln.strip()
-    if not ln:
-        return current_production, False
-    
-    if ':' in ln:
-        left, rights = ln.split(':', 1)
-        left = left.strip()
-        #current_production = left
-        rights = rights.strip()
-        for right in rights.split(' '):
-            right = right.strip()
-            Producer.append(current_production, right.split())
-    # elif ln[0] == '|':
-    #     rights = ln[1:].strip()
-    #     for right in rights.split('|'):
-    #         right = right.strip()
-    #         producer_list.append((current_production, right.split()))
-    # elif ln == ';':
-    #     current_production = None
-    # return current_production, True
+    ln_=ln[1:]
+    ln_=ln_strip()
+
+    rights = ln_
+    rights = rights.strip()
+    for right in rights.split(' '):
+        right = right.strip()
+        Producer.append((current_production, right.split()))
 
 def init_all(filename):
     current_production = None
@@ -100,16 +59,13 @@ def init_all(filename):
                 i+=1
             elif '%%' in lines[i]:
                 i+=1
-                break
-            else:
-                if ' ' not in lines[i]:
-                    current_production=lines[i]
-                    j+=1
-                parse_production(lines[j+i],current_production)
-            
-            
-
-        
+                continue
+            # else:
+            #     if ' ' not in lines[i]:
+            #         current_production=lines[i]
+            #         j+=1
+            #     parse_production(lines[j+i],current_production)
+            #     i=i+j+1
 
 def main():
     filename = "./yacc/test.txt"
