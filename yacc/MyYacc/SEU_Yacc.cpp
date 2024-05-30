4b3912138913c7d04351fe91c0264a0dff1ce5b6
GreatSlove <505909828@qq.com>
2024-05-30 14:55:02 +0800

    little alter

--- yacc/MyYacc/SEU_Yacc.cpp
+++ yacc/MyYacc/SEU_Yacc.cpp
@@ -8,12 +8,39 @@
 //#include "CodeGeneration.h"
 extern map<int, set<int> > firstMap;
 
+
+void PrintGlobalProductionVec() {
+    for (size_t i = 0; i < GlobalProductionVec.size(); ++i) {
+        // 打印数字
+        cout << i + 1 << ": ";
+        
+        // 获取产生式
+        const auto &production = GlobalProductionVec[i];
+        const string &left = production.first;
+        const vector<string> &right = production.second;
+        
+        // 打印左部
+        cout << left << " -> ";
+        
+        // 打印右部
+        for (size_t j = 0; j < right.size(); ++j) {
+            cout << right[j];
+            if (j < right.size() - 1) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
 using namespace std;
 int main() {
 
     string filepath = "./yacc.y";
     Parse_Yacc(filepath,GlobalSymbolVec,GlobalProductionVec,GlobalProducerVec,FuncVec);
 
+
+    PrintGlobalProductionVec();
     cout<<"\n------------Parse yacc.y.........done\n";
 
     Collection LR1;
